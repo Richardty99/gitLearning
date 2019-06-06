4e329d65ab2925e8fe2b69c00693612724f074ad
Richard <1227670121@qq.com>
2019-06-06 16:44:24 +0800

    Redis

---
+++ demo1/src/main/java/com/example/demo/base/redis/Redis.java
@@ -0,0 +1,12 @@
+package com.example.demo.base.redis;
+
+/**
+ * @Auther: HX001
+ * @Date: 2019/5/27 15:02
+ * @Description:
+ */
+public class Redis {
+    public static void main(String[] args){
+        //连接到
+    }
+}
